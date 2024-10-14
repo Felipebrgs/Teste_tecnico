14de6b49c8e15816155b8ecf53e4a355ff11de0d
Felipe <felipebrgess@gmail.com>
2024-10-14 11:15:37 -0300

    first commit

---
+++ teste_tecnico.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    a, b = 0, 1
+    while a < n:
+        a, b = b, a + b
+    return a == n
+
+num = int(input("Informe um número: "))
+if fibonacci(num):
+    print(f"{num} pertence à sequência de Fibonacci.")
+else:
+    print(f"{num} não pertence à sequência de Fibonacci.")
